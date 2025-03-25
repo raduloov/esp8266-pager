8a870162968fa2c3a5fe50e189cbf984f2d16f9d
raduloov <raduloov@gmail.com>
2025-03-25 16:32:59 +0200

    add express server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", function connection(ws) {
+  console.log("A new client connected!");
+  ws.send("Welcome client!");
+
+  ws.on("message", function incoming(message, isBinary) {
+    console.log("received: %s", message);
+
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message, { binary: isBinary });
+      }
+    });
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
